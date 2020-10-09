cd1835b4ba3001738137d25e9bb0d2838cc127db
Lawrence Artl <lawrence.artl@gmail.com>
2020-10-09 15:08:30 -0700

    Added exercise 6, classes trial

---
+++ BasicExercise06/People/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicExercise06.People
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public char Sex;
+        public bool HasPet;
+
+
+        public string Greeting()
+        {
+            if (HasPet == true)
+                return "How is your pet?";
+            else
+                return "How are you?";
+        }
+
+    }
+}
